400db76693c4bbfc88bcaeb45af8521a4b642e50
Gordon Hall <gordon@gordonwritescode.com>
2017-08-14 11:54:51 -0400

    cleaning up/fixing tests

--- test/directory.unit.js
+++ test/directory.unit.js
@@ -6,6 +6,7 @@ const { randomBytes } = require('crypto');
 const { expect } = require('chai');
 const Directory = require('../lib/directory');
 const http = require('http');
+const getDatabase = require('./fixtures/database');
 
 
 describe('@class Directory', function() {
@@ -13,9 +14,29 @@ describe('@class Directory', function() {
   let directory;
 
   before((done) => {
-    directory = new Directory({}, {});
-    directory.listen(0);
-    done();
+    getDatabase((err, database) => {
+      directory = new Directory({ database }, {});
+      directory.listen(0);
+
+      let profile = new database.PeerProfile({
+        identity: '00000000000000000000',
+        contact: {
+          hostname: 'test.onion',
+          port: 443,
+          protocol: 'https:',
+          xpub: '{xpubkey}',
+          index: 0,
+          agent: 'orc-test/linux'
+        },
+        capacity: {
+          allocated: 2000,
+          available: 1000,
+          timestamp: Date.now()
+        }
+      });
+
+      profile.save(err => done(err));
+    });
   });
 
   it('should respond with the directory contents', function(done) {
@@ -27,13 +48,13 @@ describe('@class Directory', function() {
         data = JSON.parse(data)[0];
         expect(data.capacity.allocated).to.equal(2000);
         expect(data.capacity.available).to.equal(1000);
-        expect(data.contact[0]).to.equal('{identity 1}');
-        expect(data.contact[1].hostname).to.equal('test.onion');
-        expect(data.contact[1].port).to.equal(443);
-        expect(data.contact[1].protocol).to.equal('https:');
-        expect(data.contact[1].xpub).to.equal('{xpubkey}');
-        expect(data.contact[1].index).to.equal(0);
-        expect(data.contact[1].agent).to.equal('orc-test/linux');
+        expect(data.identity).to.equal('00000000000000000000');
+        expect(data.contact.hostname).to.equal('test.onion');
+        expect(data.contact.port).to.equal(443);
+        expect(data.contact.protocol).to.equal('https:');
+        expect(data.contact.xpub).to.equal('{xpubkey}');
+        expect(data.contact.index).to.equal(0);
+        expect(data.contact.agent).to.equal('orc-test/linux');
         done();
       });
     });
---
+++ test/fixtures/database.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Database = require('../../lib/database');
+
+
+module.exports = function(callback) {
+  module.exports._database.connection.close(() => {
+    module.exports._database = new Database('mongodb://localhost/__orc-test');
+
+    module.exports._database.on('open', () => {
+      module.exports._database.connection.dropDatabase((err) => {
+        callback(err, module.exports._database);
+      });
+    });
+    module.exports._database.on('error', callback);
+  });
+};
+
+module.exports._database = {
+  connection: { close: (cb) => cb() }
+};
