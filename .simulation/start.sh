64e13ef774cd613a5e5aa375408344c40b033606
Gordon Hall <gordon@gordonwritescode.com>
2017-11-30 17:57:38 -0500

    replace shadow with chutney

--- .simulation/start.sh
+++ .simulation/start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# start shadow tor simulation
-/root/.shadow/bin/shadow /root/shadow-tor/shadow.config.xml #> /dev/null 2>&1 &
+# start chutney tor simulation
+/root/chutney/chutney start /root/chutney/networks/basic
 
 # start some orc providers
-#node /root/orc/bin/orcd.js --datadir /root/orc-providers/1 --daemon
-#node /root/orc/bin/orcd.js --datadir /root/orc-providers/2 --daemon
-#node /root/orc/bin/orcd.js --datadir /root/orc-providers/3 --daemon
+node /root/orc/bin/orcd.js --datadir /root/orc-providers/1 --daemon
+node /root/orc/bin/orcd.js --datadir /root/orc-providers/2 --daemon
+node /root/orc/bin/orcd.js --datadir /root/orc-providers/3 --daemon
 
 # start our main orc interface
-#node /root/orc/bin/ord.js 
+node /root/orc/bin/ord.js 
