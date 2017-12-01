a06666d2a334fdbb93e9f9f5a088aaaf2570937b
Gordon Hall <gordon@gordonwritescode.com>
2017-11-30 19:14:53 -0500

    add simulation files

--- .simulation/start.sh
+++ .simulation/start.sh
@@ -3,10 +3,16 @@
 # start chutney tor simulation
 /root/chutney/chutney start /root/chutney/networks/basic
 
+# cleanup pid files
+rm /root/.config/orcd/orcd.pid
+rm /root/providers/1/orcd.pid
+rm /root/providers/2/orcd.pid
+rm /root/providers/3/orcd.pid
+
 # start some orc providers
-node /root/orc/bin/orcd.js --datadir /root/orc-providers/1 --daemon
-node /root/orc/bin/orcd.js --datadir /root/orc-providers/2 --daemon
-node /root/orc/bin/orcd.js --datadir /root/orc-providers/3 --daemon
+node /root/orc/bin/orcd.js --datadir /root/providers/1 --daemon 
+node /root/orc/bin/orcd.js --datadir /root/providers/2 --daemon
+node /root/orc/bin/orcd.js --datadir /root/providers/3 --daemon 
 
 # start our main orc interface
-node /root/orc/bin/ord.js 
+node /root/orc/bin/orcd.js 
