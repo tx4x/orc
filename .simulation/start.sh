49647a1b2b2fe4cb7d12b55ede53e4245cd3e244
Gordon Hall <gordon@gordonwritescode.com>
2017-12-18 16:29:35 -0500

    retry contract renewal

--- .simulation/start.sh
+++ .simulation/start.sh
@@ -18,5 +18,5 @@ node /root/orc/bin/orcd.js --datadir /root/providers/3 --daemon
 
 echo "waiting 1 minute for hs descriptors to be ready..."
 sleep 60
-echo "starting orc node, exposing bridge at port 9089"
+echo "starting orc node, exposing bridge at port 10089"
 orcd_BridgeAuthenticationEnabled=0 orcd_NetworkBootstrapNodes="http://l466qntstik7falkkzxttgrtlu4u7g6yaxmyvjs2qamlzymsdsiqxdqd.onion:80" node /root/orc/bin/orcd.js 
